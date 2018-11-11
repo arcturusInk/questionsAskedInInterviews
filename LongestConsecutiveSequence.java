fdf08f447c74b88817a723e95b22da78642d0cc3
Swati B <arcturusInk@users.noreply.github.com>
2018-11-11 18:18:25 -0500

    uploading the problems, will upload solutions later

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,11 @@
+package com.company;
+/*
+* Given this dynamic programming problem in an interview. Couldn't do it then, so here is a solution now.
+Find the largest sequence of consecutive numbers that are adjacent to one another in a matrix. The matrix is a 2D matrix with sides of length n and containing m elements. Each element is a unique number in the range of [1..m] (inclusive).
+An example:
+n = 3, m = 9
+9 8 4 7 3 5 1 2 6 A longest consecutive sequence: [1,2,3]
+return a one dimensional array of intergers take in the matrix as a parameter
+*/
+public class LongestConsecutiveSequence {
+}
---
+++ Trees.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/*
+* Implement a 3-level node structure of parents and children using JavaScript and/or Java. Each node should store the parent name and a list of children. Create some node data, then using this, represent the data as a tree structure on a web page. You should be able to collapse and expand the tree structure by clicking node names on the web page. You are allowed to use plain JavaScript and/or Java or your favorite library.
+* */
+public class Trees {
+}
